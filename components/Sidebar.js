129948dd1cf92d69588c3886bd4cf65c0adc12ba
rizkianakbar <rizkianakbar4@gmail.com>
2021-04-06 17:42:44 +0700

    ADD : chat

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,11 +3,41 @@ import ChatIcon from "@material-ui/icons/Chat";
 import styled from "styled-components";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
-function Sidebar() {
+import * as EmailValidator from "email-validator";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+
+const Sidebar = () => {
+  const [user] = useAuthState(auth);
+  const userChatRef = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
+  const [chatsSnapshot] = useCollection(userChatRef);
+
+  const createChat = () => {
+    const input = prompt(
+      "Please enter an email address for the user you wish to chat with"
+    );
+
+    if (!input) return null;
+    if (EmailValidator.validate(input) && input !== user.email) {
+      db.collection("chats").add({
+        users: [user.email, input],
+      });
+    }
+  };
+
+  const chatAlreadyExists = (ReceiptEmail) {
+    !!chatsSnapshot?.docs.find(
+      chat.data().user.find(() => user === receo)
+    )
+  }
+
   return (
     <Container>
       <Header>
-        <UserAvatar />
+        <UserAvatar onClick={() => auth.signOut()} />
         <IconContainer>
           <IconButton>
             <ChatIcon />
@@ -23,10 +53,11 @@ function Sidebar() {
         <SearchInput placeholder="Search in chats" />
       </Search>
 
-      <SidebarButton>Start a new chat</SidebarButton>
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
+      {/* chat */}
     </Container>
   );
-}
+};
 
 export default Sidebar;
 
